d66c0b15273a09cabed695a35d741d7a62ac84d5
Sydney Miller <sbm7x5@gmail.com>
2017-02-14 18:20:36 -0600

    finished base build-a-blog assignment

--- main.py
+++ main.py
@@ -1,25 +1,72 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
+
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+#create blog post database
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    post = db.StringProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("/blog")
+
+class Blog(webapp2.RequestHandler):
+    def get(self):
+        #pull existing blog posts from database
+        blog_posts = db.GqlQuery("""SELECT * FROM BlogPost ORDER BY created DESC
+        LIMIT 5""")
+
+        t = jinja_env.get_template("blog.html")
+        content = t.render(blog_posts = blog_posts)
+        self.response.write(content)
+
+class NewPost(webapp2.RequestHandler):
+    def get(self):
+
+        t = jinja_env.get_template("newpost.html")
+        content = t.render()
+        self.response.write(content)
+
+    def post(self):
+        title = self.request.get("blog-title")
+        post = self.request.get("blog-post")
+
+        if title and post:
+            blog = BlogPost(title = title, post = post)
+            blog.put()
+            blog_id = blog.key().id()
+            self.redirect("/blog/%s"% blog_id)
+        else:
+            error = "You must enter both a title and post content."
+            t = jinja_env.get_template("newpost.html")
+            content = t.render(error = error)
+            self.response.write(content)
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def get(self, id):
+        post = BlogPost.get_by_id(int(id))
+        title = post.title
+        body = post.post
+
+        if not post:
+            self.renderError(400)
+            return
+
+        t = jinja_env.get_template("singlepost.html")
+        content = t.render(title = title, body = body)
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler),
+    ('/', Index),
+    ('/blog', Blog),
+    ('/newpost', NewPost)
 ], debug=True)
